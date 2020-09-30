021bf1629b21ef6e4b534092697b0286e4a1d7a0
Edward Easling <edwardeasling@gmail.com>
2020-09-30 18:48:47 -0400

    Fix webpack mode setting

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
+    mode: 'none',
     entry: './frontend/music_base.jsx',
     output: {
         path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
